674f892158506daaf4c860fd62ff67ab39f448d9
kai <kai860115@gmail.com>
2021-06-06 23:11:10 +0800

    update download.sh

--- hw3/download.sh
+++ hw3/download.sh
@@ -1,3 +1,16 @@
 wget https://www.dropbox.com/s/qkjn0vbee90mxy1/pytorch_model.bin?dl=1 -O ./mt5-summarization/pytorch_model.bin
 
 python tw_rouge/twrouge.py
+
+python -c "
+import nltk
+from filelock import FileLock
+from transformers.file_utils import is_offline_mode
+try:
+    nltk.data.find(\"tokenizers/punkt\")
+except (LookupError, OSError):
+    if is_offline_mode():
+        raise LookupError(\"Offline mode: run this script without TRANSFORMERS_OFFLINE first to download nltk data files\")
+    with FileLock(\".lock\") as lock:
+        nltk.download(\"punkt\", quiet=True)
+"
